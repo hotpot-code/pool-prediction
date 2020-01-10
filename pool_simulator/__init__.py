d8bc7957150d2c39ee2963dcc4107f8ef32785e1
Marlon Lückert <m.lueckert@me.com>
2020-01-10 13:25:29 +0100

    better sim, residuals, dynmaic process noise

--- pool_simulator/__init__.py
+++ pool_simulator/__init__.py
@@ -7,7 +7,7 @@ from .Bank import Bank
 
 class PoolSimulation():
 
-    def __init__(self, start_position = (960,540), start_angle = math.pi/2 + 0.3, start_velocity = 900, friction = 0.07):
+    def __init__(self, start_position = (960,540), start_angle = math.pi/2 + 0.3, start_velocity = 900, friction = 0.07, ms = 16):
         self.ball = Ball(start_position, 25, start_velocity, friction)
         self.ball.setRotation(start_angle)
         self.bank_left = Bank((100,100), (100, 980))
@@ -21,6 +21,10 @@ class PoolSimulation():
         self.game_objects = [self.ball, self.bank_left, self.bank_right, self.bank_bottom, self.bank_top]
         self.bank_hits = 0
         self.frame_no = 0
+        self.ms = ms
+        self.isBallMoving = True
+        self.bankDetectionRadius = 2
+        self.isBallNearBank = False
     
     def resetHitFlags(self):
         self.can_hit_left_bank = True
@@ -33,7 +37,7 @@ class PoolSimulation():
         # Get frame from video
         frame = np.zeros((1080, 1920, 3), np.uint8)
 
-        self.ball.update(16)
+        self.ball.update(self.ms)
 
         if (self.ball.position[1] + self.ball.size >= self.bank_bottom.p1[1] and self.can_hit_bottom_bank):
             angle = math.atan2(1, 0) - math.atan2(self.ball.forward[1], self.ball.forward[0])
@@ -59,10 +63,23 @@ class PoolSimulation():
             self.bank_hits += 1
             self.resetHitFlags()
             self.can_hit_right_bank = False
+
+        self.isBallNearBank = False
+        if self.ball.position[1] + self.ball.size * self.bankDetectionRadius >= self.bank_bottom.p1[1]:
+            self.isBallNearBank = True
+        if self.ball.position[1] - self.ball.size * self.bankDetectionRadius <= self.bank_top.p1[1] :
+            self.isBallNearBank = True
+        if self.ball.position[0] - self.ball.size * self.bankDetectionRadius <= self.bank_left.p1[0]:
+            self.isBallNearBank = True
+        if self.ball.position[0] + self.ball.size * self.bankDetectionRadius >= self.bank_right.p1[0]:
+            self.isBallNearBank = True
         
         for game_object in self.game_objects:
             game_object.render(frame)
 
         velocity_vector = (int(self.ball.forward[0] * self.ball.velocity), int(self.ball.forward[1] * self.ball.velocity))
+
+        if self.ball.velocity == 0:
+            self.isBallMoving = False
 
         return (frame, self.ball.position, velocity_vector)
--- simulation.py
+++ simulation.py
@@ -1,28 +1,44 @@
 import numpy as np
-import cv2 
+import cv2
+import math
 
 from pool_simulator import PoolSimulation
 from filter.filter_constant_velocity import MyFilter
 
-kalman = MyFilter(0.018, 300.0, 1.0)
-sim = PoolSimulation(start_velocity = 900)
+noise = 10
+update_time_in_ms = 16
+process_noise = 950
+dynamic_process_noise = 8000
+
+kalman = MyFilter(update_time_in_ms / 1000, process_noise, noise * 1.1)
+kalman_dynamic = MyFilter(update_time_in_ms / 1000, process_noise, noise * 1.1)
+sim = PoolSimulation(start_velocity = 900, ms=update_time_in_ms)
 
 points = list()
+noised_points = list()
 filtered_points = list()
+dynamic_filtered_points = list()
 
 frame_no = 0
-while frame_no < 1000:
+while sim.isBallMoving:
     frame, position, velocity = sim.update()
     #cv2.line(frame, position, (position[0] + velocity[0], position[1] + velocity[1]), (255,0,0), 4)
     
-    noised_position = (int(position[0] + np.random.randn() * 1), int(position[1] + np.random.randn() * 1))
+    noised_position = (int(position[0] + np.random.randn() * noise), int(position[1] + np.random.randn() * noise))
+
+    if sim.isBallNearBank:
+        kalman_dynamic.process_noise = dynamic_process_noise
+    else:
+        kalman_dynamic.process_noise = process_noise
+
     filtered = kalman.dofilter(noised_position[0], noised_position[1])
+    filtered_dynamic = kalman_dynamic.dofilter(noised_position[0], noised_position[1])
 
     last_point = None
-    points.append(noised_position)
-    if len(points) > 1:
+    noised_points.append(noised_position)
+    if len(noised_points) > 1:
         last_point = None
-        for point in points:
+        for point in noised_points:
             if last_point is not None:
                 cv2.line(frame, (int(last_point[0]),int(last_point[1])), (int(point[0]),int(point[1])), (0,0,255), 2)
             last_point = point
@@ -36,8 +52,39 @@ while frame_no < 1000:
                 cv2.line(frame, (int(last_point[0]),int(last_point[1])), (int(point[0]),int(point[1])), (255,255,255), 2)
             last_point = point
 
+    last_point = None
+    points.append(position)
+    if len(points) > 1:
+        last_point = None
+        for point in points:
+            if last_point is not None:
+                cv2.line(frame, (int(last_point[0]), int(last_point[1])), (int(point[0]), int(point[1])),
+                         (0, 255, 0), 2)
+            last_point = point
+
+    dynamic_filtered_points.append(filtered_dynamic)
+
     cv2.circle(frame, noised_position, 10, (0,0,255), -1)
 
     cv2.imshow("Pool Simulation", frame)
     frame_no += 1
-    cv2.waitKey(16)
+    cv2.waitKey(1)
+
+
+def residual(points, ground_truth):
+    sum = 0
+    for i in range(0, len(points) - 1):
+        point = points[i]
+        gt = ground_truth[i]
+        distance =  math.sqrt((point[0] - gt[0])**2 + (point[1] - gt[1])**2)
+        sum += distance
+    return sum / len(points)
+
+print("filter: ")
+print(residual(filtered_points, points))
+print("filter with dynamic process noise: ")
+print(residual(dynamic_filtered_points, points))
+print("no filter: ")
+print(residual(noised_points, points))
+print("truth: ")
+print(residual(points, points))
